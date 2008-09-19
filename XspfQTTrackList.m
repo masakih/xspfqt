92e81c5e9aae480cf592205455b5659a5b433305
Hori,Masaki <masakih@hm.h555.net>
2008-09-19 15:15:55 +0000

    現在再生中のtrackを持つ事で、挿入、削除を単純化した。
    git-svn-id: svn+ssh://macmini/usr/local/svnrepos/XspfQT/XspfQT@87 26ab9374-fca1-4da0-aed3-2e79c8066918

--- XspfQTTrackList.m
+++ XspfQTTrackList.m
@@ -12,31 +12,31 @@
 @implementation XspfQTTrackList
 - (id)initWithXMLElement:(NSXMLElement *)element
 {
-	self = [super init];
-	
-	NSArray *elems = [element elementsForName:@"title"];
-	if(elems && [elems count] != 0) {
-		NSString *t = [[elems objectAtIndex:0] stringValue];
-		[self setTitle:t];
-	}
-	
-	elems = [element elementsForName:@"track"];
-	if(!elems) {
-		[self release];
-		return nil;
-	}
-	tracks = [[NSMutableArray alloc] init];
-	
-	unsigned i, count;
-	for(i = 0, count = [elems count]; i < count; i++) {
-		NSXMLElement *trackElem = [elems objectAtIndex:i];
-		XspfQTComponent *track = [XspfQTComponent xspfComponemtWithXMLElement:trackElem];
-		if(track) {
-			[self addChild:track];
+	if(self = [super init]) {
+		[self setCurrentIndex:NSNotFound];
+		
+		NSArray *elems = [element elementsForName:@"title"];
+		if(elems && [elems count] != 0) {
+			NSString *t = [[elems objectAtIndex:0] stringValue];
+			[self setTitle:t];
+		}
+		
+		elems = [element elementsForName:@"track"];
+		if(!elems) {
+			[self release];
+			return nil;
+		}
+		tracks = [[NSMutableArray alloc] init];
+		
+		unsigned i, count;
+		for(i = 0, count = [elems count]; i < count; i++) {
+			NSXMLElement *trackElem = [elems objectAtIndex:i];
+			XspfQTComponent *track = [XspfQTComponent xspfComponemtWithXMLElement:trackElem];
+			if(track) {
+				[self addChild:track];
+			}
 		}
 	}
-//	[self setCurrentIndex:NSNotFound];
-	currentIndex = NSNotFound;
 	
 	return self;
 }
@@ -83,87 +83,45 @@
 						  context:context];
 }
 
-// this mothod not check toTrack and fromTrack are in tracks array.
-// Do not call directly.
-- (void)changeObserveFrom:(XspfQTComponent *)fromTrack to:(XspfQTComponent *)toTrack
-{
-//	NSLog(@"Change From %@ to %@.", fromTrack, toTrack);
-	
-	if(fromTrack == toTrack) return;
-	
-	@try {
-		[toTrack addObserver:self
-				  forKeyPath:@"isPlayed"
-					 options:NSKeyValueObservingOptionNew
-					 context:NULL];
-	}
-	@catch (NSException *ex) {
-		NSLog(@"Caught exception(%@).",ex);
-		if(![[ex name] isEqualToString:NSRangeException]) {
-			NSLog(@"Exception ### named %@", [ex name]);
-			@throw;
-		}
-	}
-	@catch (id ex2) {
-		NSLog(@"Caught exception(%@).",ex2);
-	}
-	
-	
-	@try {
-		[fromTrack removeObserver:self forKeyPath:@"isPlayed"];
-	}
-	@catch (NSException *ex) {
-		NSLog(@"Caught exception(%@).",ex);
-		NSLog(@"FromTrack is %@", fromTrack);
-		if(![[ex name] isEqualToString:NSRangeException]) {
-			NSLog(@"Exception ### named %@", [ex name]);
-			@throw;
-		}
-	}
-	@catch (id ex2) {
-		NSLog(@"Caught exception(%@).",ex2);
-		NSLog(@"FromTrack is %@", fromTrack);
-	}
-	@finally {
-//		NSLog(@"Prev -> %@\nNew -> %@", fromTrack, toTrack);
-		[self willChangeValueForKey:@"isPlayed"];
-		[fromTrack deselect];
-		[toTrack select];
-		[self didChangeValueForKey:@"isPlayed"];
-	}
-}	
 
-- (void)setSelectionIndex:(unsigned)index
+- (void)setSelectionIndex:(NSUInteger)index
 {
 	[self setCurrentIndex:index];
 }
 - (void)setCurrentIndex:(unsigned)index
 {
-	unsigned prev;
-	
-	if(index < 0) return;
 	if([tracks count] <= index && index != NSNotFound) return;
 	
+	if(index == currentIndex && index != NSNotFound) {
+		id new = [tracks objectAtIndex:index];
+		if(new == selectedComponent) {
+			return;
+		}
+	}
+	
+	XspfQTComponent *newSelection = nil;
+	if(index != NSNotFound) {
+		newSelection = [tracks objectAtIndex:index];
+	}
+	if(selectedComponent != newSelection) {
+		[selectedComponent removeObserver:self  forKeyPath:@"isPlayed"];
+		[selectedComponent deselect];
+		
+		selectedComponent = newSelection;
+		[selectedComponent addObserver:self
+							forKeyPath:@"isPlayed"
+							   options:NSKeyValueObservingOptionNew
+							   context:NULL];
+		[selectedComponent select];
+	}
+	
+	[self willChangeValueForKey:@"isPlayed"];
 	[self willChangeValueForKey:@"qtMovie"];
 	[self willChangeValueForKey:@"currentTrack"];
-	prev = currentIndex;
 	currentIndex = index;
 	[self didChangeValueForKey:@"currentTrack"];
 	[self didChangeValueForKey:@"qtMovie"];
-	
-	XspfQTComponent *t= nil;
-	@try {
-		t = [tracks objectAtIndex:prev];
-	}
-	@catch (id ex) {
-		if(![[ex name] isEqualToString:NSRangeException]) {
-			NSLog(@"Exception ### named %@", [ex name]);
-			@throw;
-		}
-	}
-	XspfQTComponent *t2 = [self currentTrack];
-	
-	[self changeObserveFrom:t to:t2];
+	[self didChangeValueForKey:@"isPlayed"];
 }
 - (unsigned)currentIndex
 {
@@ -204,14 +162,8 @@
 	[self didChangeValueForKey:@"children"];
 	
 	// 選択アイテムの前に挿入される場合は、currentIndexを変更する。
-	// ### CAUTION ###
-	// this line directly change currentIndex.
 	if(index <= currentIndex) {
-		[self willChangeValueForKey:@"qtMovie"];
-		[self willChangeValueForKey:@"currentTrack"];
-		currentIndex++;
-		[self didChangeValueForKey:@"currentTrack"];
-		[self didChangeValueForKey:@"qtMovie"];
+		[self setCurrentIndex:currentIndex + 1];
 	}
 }
 // primitive.
@@ -221,15 +173,6 @@
 	if(![tracks containsObject:child]) return;
 	
 	NSUInteger index = [tracks indexOfObject:child];
-	// for archive to original
-//	child = [tracks objectAtIndex:index]; in XspfQTPlayListWindowController
-	
-	BOOL isSelectedItem = [child isSelected];
-	BOOL mustChangeSelection = NO;
-	
-	if(index <= currentIndex) {
-		mustChangeSelection = YES;
-	}
 	
 	[self willChangeValueForKey:@"children"];
 	[[child retain] autorelease];
@@ -237,10 +180,8 @@
 	[tracks removeObject:child];
 	[self didChangeValueForKey:@"children"];
 	
-//	NSLog(@"current %u >= remove %u ... %@", currentIndex, index, mustChangeSelection ? @"YES": @"NO");
-	if(mustChangeSelection) {
-		// ### CAUTION ###
-		// this line directly change currentIndex.
+	// 再生位置の変更
+	if(index <= currentIndex) {
 		NSUInteger newIndex = currentIndex - 1;
 		if(currentIndex == 0) {
 			if([self childrenCount] == 0) {
@@ -249,21 +190,7 @@
 				newIndex = 0;
 			}
 		}
-		[self willChangeValueForKey:@"qtMovie"];
-		[self willChangeValueForKey:@"currentTrack"];
-		currentIndex = newIndex;
-		[self didChangeValueForKey:@"currentTrack"];
-		[self didChangeValueForKey:@"qtMovie"];
-		
-//		NSLog(@"is selected ? %@", isSelectedItem ? @"YES": @"NO");
-		
-		id newSelection = nil;
-		id oldSelection = nil;
-		if(isSelectedItem) {
-			oldSelection = child;
-			newSelection = [self currentTrack];
-		}
-		[self changeObserveFrom:oldSelection to:newSelection];
+		[self setCurrentIndex:newIndex];
 	}
 }
 
@@ -285,10 +212,7 @@
 
 - (XspfQTComponent *)currentTrack
 {
-	if([tracks count] > currentIndex) {
-		return [tracks objectAtIndex:currentIndex];
-	}
-	return nil;
+	return selectedComponent;
 }
 
 - (QTMovie *)qtMovie
