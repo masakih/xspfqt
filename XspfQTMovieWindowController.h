d41f99ab18c6b57a7c8c5c01d65fbc4db76a1cd3
Hori,Masaki <masakih@hm.h555.net>
2008-09-21 14:27:11 +0000

    IBAction, turnUpVolume:とturnDownVolume:の宣言を公開した。
    git-svn-id: svn+ssh://macmini/usr/local/svnrepos/XspfQT/XspfQT@102 26ab9374-fca1-4da0-aed3-2e79c8066918

--- XspfQTMovieWindowController.h
+++ XspfQTMovieWindowController.h
@@ -28,6 +28,8 @@
 	NSSize windowSizeWithoutQTView;
 }
 
+- (IBAction)turnUpVolume:(id)sender;
+- (IBAction)turnDownVolume:(id)sender;
 - (IBAction)togglePlayAndPause:(id)sender;
 - (IBAction)toggleFullScreenMode:(id)sender;
 - (IBAction)forwardTagValueSecends:(id)sender;
