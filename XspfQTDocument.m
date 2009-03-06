b0dcc1c22dba78156fb95579a9929b81a7821b3d
Hori,Masaki <masakih@hm.h555.net>
2009-03-06 13:42:35 +0000

    Matroska Videoの読み込みに対応。
    git-svn-id: svn+ssh://macmini/usr/local/svnrepos/XspfQT/XspfQT@162 26ab9374-fca1-4da0-aed3-2e79c8066918

--- XspfQTDocument.m
+++ XspfQTDocument.m
@@ -59,7 +59,8 @@ NSString *XspfQTDocumentWillCloseNotification = @"XspfQTDocumentWillCloseNotific
 {
 	*outError = nil;
 	
-	if(![typeName isEqualToString:@"QuickTime Movie"]) {
+	if(![typeName isEqualToString:@"QuickTime Movie"]
+	   || ![typeName isEqualToString:@"Matroska Video"]) {
 		NSData *data = [self dataFromURL:absoluteURL error:outError];
 		if(!data) return NO;
 		
