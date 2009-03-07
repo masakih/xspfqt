f0b9fec5a465f9fdeaadc98aa279da63e3ea9f9d
masaki <masaki@26ab9374-fca1-4da0-aed3-2e79c8066918>
2009-03-07 13:05:16 +0000

    矢印キー、ピリオドキーでの操作を無効にするためにQTMovieViewのサブクラスを作成。
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/xspfqt/XspfQT@164 26ab9374-fca1-4da0-aed3-2e79c8066918

---
+++ XspfQTMovieView.h
@@ -0,0 +1,17 @@
+//
+//  XspfQTMovieView.h
+//  XspfQT
+//
+//  Created by Hori,Masaki on 09/03/07.
+//  Copyright 2009 masakih. All rights reserved.
+//
+
+#import <QTKit/QTKit.h>
+
+
+@interface XspfQTMovieView : QTMovieView
+{
+
+}
+
+@end
