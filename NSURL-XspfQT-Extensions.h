637d44bc35b44ae5797096a43bd12495f43bc49a
Hori,Masaki <masakih@hm.h555.net>
2009-03-15 00:57:07 +0000

    NSURLにfile:///path と file://localhost/pathを等価と見なすメソッドをカテゴリで追加。
    git-svn-id: svn+ssh://macmini/usr/local/svnrepos/XspfQT/XspfQT@178 26ab9374-fca1-4da0-aed3-2e79c8066918

---
+++ NSURL-XspfQT-Extensions.h
@@ -0,0 +1,16 @@
+//
+//  NSURL-XspfQT-Extensions.h
+//  XspfQT
+//
+//  Created by Hori,Masaki on 09/03/15.
+//  Copyright 2009 masakih. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface NSURL (XspfQT_Extensions)
+
+- (BOOL)isEqualUsingLocalhost:(NSURL *)other;
+
+@end
