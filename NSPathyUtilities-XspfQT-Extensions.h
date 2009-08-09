5e0ebf82740690057f984d719b4d9d237008b036
Hori,Masaki <masakih@hm.h555.net>
2009-08-09 10:18:59 +0000

    NSString, NSDataからAliasを生成、解決するNSPathyUtilities-XspfQT-Extensionsを追加。
    git-svn-id: svn+ssh://macmini/usr/local/svnrepos/XspfQT/XspfQT@210 26ab9374-fca1-4da0-aed3-2e79c8066918

---
+++ NSPathyUtilities-XspfQT-Extensions.h
@@ -0,0 +1,18 @@
+//
+//  NSPathyUtilities-XspfQT-Extensions.h
+//  XspfQT
+//
+//  Created by Hori,Masaki on 09/07/26.
+//  Copyright 2009 masakih. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface NSString (XspfQT_Extensions)
+- (NSData *)aliasData;
+@end
+
+@interface NSData (XspfQT_Extensions)
+- (NSString *)resolvedPath;
+@end
---
+++ NSPathyUtilities-XspfQT-Extensions.m
@@ -0,0 +1,82 @@
+//
+//  NSPathyUtilities-XspfQT-Extensions.m
+//  XspfQT
+//
+//  Created by Hori,Masaki on 09/07/26.
+//  Copyright 2009 masakih. All rights reserved.
+//
+
+#import "NSPathyUtilities-XspfQT-Extensions.h"
+
+
+@implementation NSString (XspfQT_Extensions)
+- (NSData *)aliasData
+{
+	NSData *result = nil;
+	
+	NSURL *url;
+	FSRef ref;
+	AliasHandle alias;
+	SInt8 handleState;
+	BOOL res;
+	OSErr error;
+	
+	url = [NSURL fileURLWithPath:self];
+	if(!url) {
+		return nil;
+	}
+	
+	res = CFURLGetFSRef((CFURLRef)url , &ref);
+	if(!res) {
+		return nil;
+	}
+	
+	error = FSNewAlias(nil, &ref, &alias);
+	if(error != noErr) {
+		return nil;
+	}
+	
+	handleState = HGetState((Handle)alias);
+	HLock((Handle)alias);
+	result = (NSData *)CFDataCreate(kCFAllocatorDefault, (const UInt8 *)*alias, GetHandleSize((Handle)alias));
+	HSetState((Handle)alias, handleState);
+	DisposeHandle((Handle)alias);
+	
+	return result;
+}
+@end
+
+@implementation NSData (XspfQT_Extensions)
+- (NSString *)resolvedPath
+{
+	NSString *result = nil;
+	
+	AliasHandle alias;
+	FSRef ref;
+	CFURLRef url;
+	SInt8 handleState;
+	Boolean wasChanged = NO;
+	OSErr error;
+	
+	alias = (AliasHandle)NewHandle([self length]);
+	handleState = HGetState((Handle)alias);
+	HLock((Handle)alias);
+	BlockMoveData([self bytes], *alias, [self length]);
+	HSetState((Handle)alias, handleState);
+	
+	error = FSResolveAlias(NULL, alias, &ref, &wasChanged);
+	DisposeHandle((Handle)alias);
+	if(error != noErr) {
+		return nil;
+	}
+	
+	url = CFURLCreateFromFSRef(NULL, &ref);
+	if(url) {
+		result = (NSString *)CFURLCopyFileSystemPath(url, kCFURLPOSIXPathStyle);
+	}
+	CFRelease(url);
+	
+	return [result autorelease];
+}
+	
+@end
