824a30de7ea6e24531215107fade5e0b792df34d
masaki <masaki@26ab9374-fca1-4da0-aed3-2e79c8066918>
2009-10-29 10:50:27 +0000

    [New] BSSUtilをBSSpotlighterから取り込み。
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/xspfqt/XspfQT@288 26ab9374-fca1-4da0-aed3-2e79c8066918

---
+++ BSSUtil.m
@@ -0,0 +1,167 @@
+/*
+ *  BSSUtil.c
+ *  BSSpotlighter
+ *
+ *  Created by Hori,Masaki on 06/12/16.
+ *  Copyright 2006 masakih. All rights reserved.
+ *
+ */
+
+#import "BSSUtil.h"
+#import "stdarg.h"
+
+#import <Foundation/Foundation.h>
+#import "NSAppleEventDescriptor-Extensions.h"
+
+
+#import <AppKit/NSWorkspace.h>
+
+NSString *BSSLogForceWrite = @"BSSLogForceWrite";
+
+void BSSLog(NSString *format, ...)
+{
+	va_list ap;
+	
+	va_start(ap, format);
+	BSSLogv(format, ap);
+	va_end(ap);
+}
+
+#ifdef DEBUG
+void BSSLogv(NSString *format, va_list args)
+{
+	NSLogv(format, args);
+}
+#else
+void BSSLogv(NSString *format, va_list args)
+{
+	NSUserDefaults *def = [NSUserDefaults standardUserDefaults];
+	if([def boolForKey:BSSLogForceWrite]) {
+		NSLogv(format, args);
+	}
+}
+#endif
+
+OSStatus activateFinderOnlyFrontWindow()
+{
+	[[NSWorkspace sharedWorkspace] launchApplication:@"Finder"];
+	return noErr;
+}
+
+inline static NSAppleEventDescriptor *fileDescriptor(NSString *filePath)
+{
+	NSAppleEventDescriptor *fileDesc;
+	NSAppleEventDescriptor *fileNameDesc;
+	NSURL *fileURL = [NSURL fileURLWithPath:filePath];
+	const char *fileURLCharP;
+	
+	fileURLCharP = [[fileURL absoluteString] fileSystemRepresentation];
+	fileNameDesc = [NSAppleEventDescriptor descriptorWithDescriptorType:typeFileURL
+																  bytes:fileURLCharP
+																 length:strlen(fileURLCharP)];
+	
+	fileDesc = [NSAppleEventDescriptor objectSpecifierWithDesiredClass:cFile
+															 container:nil
+															   keyForm:formName
+															   keyData:fileNameDesc];
+	return fileDesc;
+}
+OSStatus openInFinderWithPath(NSString *filePath)
+{
+	NSAppleEventDescriptor *ae;
+	NSAppleEventDescriptor *targetDesc;
+	OSStatus err;
+	
+	targetDesc = [NSAppleEventDescriptor targetDescriptorWithAppName:@"Finder"];
+	if(!targetDesc) {
+		BSSLog(@"Can NOT create targetDesc.");
+		return kBSSUtilCanNotCreateTragetDescErr;
+	}
+	
+	ae = [NSAppleEventDescriptor appleEventWithEventClass:kAEMiscStandards
+												  eventID:kAEMakeObjectsVisible
+										 targetDescriptor:targetDesc
+												 returnID:kAutoGenerateReturnID
+											transactionID:kAnyTransactionID];
+	{
+		NSAppleEventDescriptor *fileDesc = fileDescriptor(filePath);
+		
+		[ae setParamDescriptor:fileDesc
+					forKeyword:keyDirectObject];
+	}
+	if(!ae) {
+		BSSLog(@"Can NOT create AppleEvent.");
+		return kBSSUtilCanNotCreateAppleEventErr;
+	}
+	
+	@try {
+		err = [ae sendAppleEventWithMode:kAENoReply | kAENeverInteract
+						  timeOutInTicks:kAEDefaultTimeout
+								  replay:NULL];
+	}
+	@catch (NSException *ex) {
+		if(![[ex name] isEqualTo:HMAEDescriptorSendingNotAppleEventException]) {
+			@throw;
+		}
+	}
+	@finally {
+		if( err != noErr ) {
+			BSSLog(@"AESendMessage Error. Error NO is %d.", err );
+		}
+	}
+	
+	return activateFinderOnlyFrontWindow();
+}
+
+
+OSStatus openInfomationInFinderWithPath(NSString *filePath)
+{
+	NSAppleEventDescriptor *ae;
+	NSAppleEventDescriptor *targetDesc;
+	OSStatus err;
+	
+	targetDesc = [NSAppleEventDescriptor targetDescriptorWithAppName:@"Finder"];
+	if(!targetDesc) {
+		BSSLog(@"Can NOT create targetDesc.");
+		return kBSSUtilCanNotCreateTragetDescErr;
+	}
+	
+	ae = [NSAppleEventDescriptor appleEventWithEventClass:kCoreEventClass
+												  eventID:kAEOpenDocuments
+										 targetDescriptor:targetDesc
+												 returnID:kAutoGenerateReturnID
+											transactionID:kAnyTransactionID];
+	{
+		NSAppleEventDescriptor *fileDesc = fileDescriptor(filePath);
+		NSAppleEventDescriptor *fileInfoDesc = [NSAppleEventDescriptor
+							objectSpecifierWithDesiredClass:cProperty
+												  container:fileDesc
+													keyForm:cProperty
+													keyData:[NSAppleEventDescriptor descriptorWithTypeCode:cInfoWindow]];
+		
+		[ae setParamDescriptor:fileInfoDesc
+					forKeyword:keyDirectObject];
+	}
+	if(!ae) {
+		BSSLog(@"Can NOT create AppleEvent.");
+		return kBSSUtilCanNotCreateAppleEventErr;
+	}
+	
+	@try {
+		err = [ae sendAppleEventWithMode:kAENoReply | kAENeverInteract
+						  timeOutInTicks:kAEDefaultTimeout
+								  replay:NULL];
+	}
+	@catch (NSException *ex) {
+		if(![[ex name] isEqualTo:HMAEDescriptorSendingNotAppleEventException]) {
+			@throw;
+		}
+	}
+	@finally {
+		if( err != noErr ) {
+			BSSLog(@"AESendMessage Error. Error NO is %d.", err );
+		}
+	}
+	
+	return noErr;
+}
