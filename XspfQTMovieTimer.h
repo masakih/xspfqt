f9bd8ea0bb0c307936ead41fccd9dbf1555aad7f
masaki <masaki@26ab9374-fca1-4da0-aed3-2e79c8066918>
2009-11-01 00:08:49 +0000

    [New] XspfQTDocumentとXspfQTMovieWindowControllerのタイマーを一元化するクラスを追加。
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/xspfqt/XspfQT@297 26ab9374-fca1-4da0-aed3-2e79c8066918

---
+++ XspfQTMovieTimer.h
@@ -0,0 +1,26 @@
+//
+//  XspfQTMovieTimer.h
+//  XspfQT
+//
+//  Created by Hori, Masaki on 09/10/31.
+//  Copyright 2009 masakih. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import "XspfQTDocument.h"
+#import "XspfQTMovieWindowController.h"
+
+
+@interface XspfQTMovieTimer : NSObject
+{
+	NSTimer *timer;
+	NSMutableArray *documents;
+	NSMutableDictionary *movieWindowControllers;
+}
+
++ (id)movieTimer;
+
+- (void)put:(XspfQTDocument *)doc;
+
+@end
