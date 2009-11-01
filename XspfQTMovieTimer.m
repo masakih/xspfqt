de5b6131a38e8699bd8d833ad97a7cef30195cee
Hori,Masaki <masakih@hm.h555.net>
2009-11-01 00:08:49 +0000

    [New] XspfQTDocumentとXspfQTMovieWindowControllerのタイマーを一元化するクラスを追加。
    git-svn-id: svn+ssh://macmini/usr/local/svnrepos/XspfQT/XspfQT@297 26ab9374-fca1-4da0-aed3-2e79c8066918

---
+++ XspfQTMovieTimer.m
@@ -0,0 +1,115 @@
+//
+//  XspfQTMovieTimer.m
+//  XspfQT
+//
+//  Created by Hori, Masaki on 09/10/31.
+//  Copyright 2009 masakih. All rights reserved.
+//
+
+#import "XspfQTMovieTimer.h"
+
+
+@implementation XspfQTMovieTimer
+
+- (id)init
+{
+	self = [super init];
+	
+	documents = [[NSMutableArray alloc] init];
+	movieWindowControllers = [[NSMutableDictionary alloc] init];
+	
+	NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
+	[nc addObserver:self
+		   selector:@selector(documentWillClose:)
+			   name:XspfQTDocumentWillCloseNotification
+			 object:nil];
+	
+	return self;
+}
+
++ (id)movieTimer
+{
+	return [[[self alloc] init] autorelease];
+}
+- (void)dealloc
+{
+	[documents release];
+	[movieWindowControllers release];
+	[timer invalidate]; timer = nil;
+	
+	NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
+	[nc removeObserver:self];
+	
+	[super dealloc];
+}
+
+- (void)makeTimer
+{
+	@synchronized(self) {
+		if(timer) return;
+		timer = [NSTimer scheduledTimerWithTimeInterval:0.5
+												 target:self
+											   selector:@selector(fire:)
+											   userInfo:NULL
+												repeats:YES];
+		[timer retain];
+	}
+}
+- (void)dropTimer
+{
+	@synchronized(self) {
+		[timer invalidate];
+		[timer release];
+		timer = nil;
+	}
+}
+
+- (void)put:(XspfQTDocument *)doc
+{
+	if(![documents containsObject:doc]) {
+		@synchronized(documents) {
+			[documents addObject:doc];
+			
+			NSArray *wControlers = [doc windowControllers];
+			for(id w in wControlers) {
+				if([w isKindOfClass:[XspfQTMovieWindowController class]]) {
+					[movieWindowControllers setObject:w forKey:[NSValue valueWithPointer:doc]];
+				}
+			}
+		}
+		
+		[self makeTimer];
+	}
+}
+
+- (void)documentWillClose:(id)notification
+{
+	id doc = [notification object];
+	if([documents containsObject:doc]) {
+		@synchronized(documents) {
+			[movieWindowControllers removeObjectForKey:[NSValue valueWithPointer:doc]];
+			[documents removeObject:doc];
+			
+			if([documents count] == 0) {
+				[self dropTimer];
+			}
+		}
+	}
+}
+
+- (void)fire:(id)t
+{
+	XspfQTDocument *doc;
+	XspfQTMovieWindowController *wc;
+	
+	@synchronized(documents) {
+		for(doc in documents) {
+			wc = [movieWindowControllers objectForKey:[NSValue valueWithPointer:doc]];
+			
+			[doc checkPreload:t];
+			[wc updateTimeIfNeeded:t];
+		}
+	}
+}
+
+@end
