a94b4f7f210a451ac7613a0409307424ced1a7d1
masakih <masakih@26ab9374-fca1-4da0-aed3-2e79c8066918>
2012-03-04 01:39:53 +0000

    [Mod] ライセンスの年を変更
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/xspfqt/XspfQT@373 26ab9374-fca1-4da0-aed3-2e79c8066918

--- XspfQTPlayListWindowController.m
+++ XspfQTPlayListWindowController.m
@@ -7,7 +7,7 @@
 
 /*
  This source code is release under the New BSD License.
- Copyright (c) 2008-2010, masakih
+ Copyright (c) 2008-2010,2012, masakih
  All rights reserved.
  
  ソースコード形式かバイナリ形式か、変更するかしないかを問わず、以下の条件を満たす場合に
@@ -29,7 +29,7 @@
  されない）直接損害、間接損害、偶発的な損害、特別損害、懲罰的損害、または結果損害につい
  て、一切責任を負わないものとします。
  -------------------------------------------------------------------
- Copyright (c) 2008-2010, masakih
+ Copyright (c) 2008-2010,2012, masakih
  All rights reserved.
  
  Redistribution and use in source and binary forms, with or without
